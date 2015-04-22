f4fbe4917303a8a3656ef73aff5227e0f6168268
eliquious <max.franks@synapse-wireless.com>
2015-04-22 00:46:06 -0500

    adding LOTS of docs. moved util functions to bottom, made VersionOneLogFile private, hopefully made the code drier.

--- wal.go
+++ wal.go
@@ -1,6 +1,7 @@
 package wallaby
 
 import (
+    "bytes"
     "io"
     "sync"
     "time"
@@ -9,34 +10,46 @@ import "os"
 import "errors"
 import "github.com/eliquious/xbinary"
 
+// ## **Log Errors**
+
 var (
-    // ErrReadIndexHeader occurs when the index header cannot be read
+    // - `ErrReadIndexHeader` occurs when the index header cannot be read
     ErrReadIndexHeader = errors.New("failed to read index header")
 
-    // ErrWriteIndexHeader occurs when the index header cannot be written
+    // - `ErrWriteIndexHeader` occurs when the index header cannot be written
     ErrWriteIndexHeader = errors.New("failed to write index header")
 
-    // ErrReadLogHeader occurs when the log header cannot be read
+    // - `ErrReadLogHeader` occurs when the log header cannot be read
     ErrReadLogHeader = errors.New("failed to read log header")
 
-    // ErrWriteLogHeader occurs when the log header cannot be written
+    // - `ErrWriteLogHeader` occurs when the log header cannot be written
     ErrWriteLogHeader = errors.New("failed to write log header")
 
-    // ErrSliceOutOfBounds occurs when index.Slice is called and the offset is larger than the size of the index.
+    // - `ErrSliceOutOfBounds` occurs when index.Slice is called and the offset is larger than the size of the index.
     ErrSliceOutOfBounds = errors.New("read offset out of bounds")
 
-    // ErrReadIndex occurs when index.Slice fails to read the records
+    // - `ErrReadIndex` occurs when index.Slice fails to read the records
     ErrReadIndex = errors.New("failed to read index records")
 
-    // ErrConfigRequired occurs when no log config is given when creating or opening a log file.
+    // - `ErrConfigRequired` occurs when no log config is given when creating or opening a log file.
     ErrConfigRequired = errors.New("log config required")
 
-    // ErrInvalidFileVersion occurs when the version in the file header is unrecognized.
+    // - `ErrInvalidFileVersion` occurs when the version in the file header is unrecognized.
     ErrInvalidFileVersion = errors.New("invalid file version")
 
-    // ErrWriteLogRecord occurs when a record fails to be written to the log
+    // - `ErrInvalidFileSignature` occurs when the signature in the file header
+    // is unrecognized.
+    ErrInvalidFileSignature = errors.New("invalid file signature")
+
+    // - `ErrWriteLogRecord` occurs when a record fails to be written to the log
     ErrWriteLogRecord = errors.New("failed to write record")
+
+    // ## **Log Variables**
+    // File signature bytes
+    LogFileSignature = []byte("LOG")
 )
+
+// ## **Log Constants**
 
 const (
     // FlagsDefault is the default boolean flags for an index file
@@ -88,6 +101,174 @@ var DefaultConfig Config = Config{
     Version:       VersionOne,
 }
 
+// ## **Open log file**
+// Open returns a `WriteAheadLog` implementation if no errors occur. If the
+// given filename already exists, the log file will try to be opened. If the
+// file format can be verified, the existing log will be returned. If the
+// file does not exist, a new log will be created with the given config.
+//
+// If the file already exists and the file version is different than the given
+// `config.Version`, the file will remain the version in which is was created.
+// In other words the file will not be updated to the newer version.
+// ###### Implementation
+func Open(filename string, config Config) (WriteAheadLog, error) {
+
+    // Determine if the given config is valid. If the given config is `nil`,
+    // a `ErrConfigRequired` error will be returned.
+    if &config == nil {
+        return nil, ErrConfigRequired
+    }
+
+    // Open the file name, creating the file if it does not already exist. The
+    // file is opened with the `APPEND` flag, which means all writes are
+    // appended to the file. Additional file modes can be given with the config.
+    file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_RDWR, config.FileMode)
+
+    // If there was an error opening the file, the open error is returned.
+    if err != nil {
+        return nil, err
+    }
+
+    // Get the file stat. The file size is gotten from this call. This helps
+    // to determine if the file already has a header or not.
+    stat, err := file.Stat()
+
+    // Return an error if the `os.FileStat` could not be retrieved. The file
+    // is closed before returning.
+    if err != nil {
+        file.Close()
+        return nil, err
+    }
+
+    // If the file size suggests the header exists, open an existing file.
+    // Otherwise create a new file based on the given config.
+    if stat.Size() >= HeaderOffset {
+        return openExisting(file, filename, config)
+    } else {
+        return createNew(file, filename, config)
+    }
+}
+
+//
+// versionOneLogFile
+type versionOneLogFile struct {
+    lock        sync.Mutex
+    fd          *os.File
+    writeCloser io.WriteCloser
+    header      FileHeader
+    index       LogIndex
+    factory     VersionOneLogRecordFactory
+    flags       uint32
+    size        int64
+    state       State
+}
+
+func (v *versionOneLogFile) Open() error {
+    v.state = OPEN
+    return nil
+}
+
+func (v *versionOneLogFile) Pipe(offset, limit uint64, writer io.Writer) error {
+    // create cursor
+    cur := v.Cursor()
+
+    // iterate over requested record range
+    for record, err := cur.Seek(offset); err != nil && record.Index() < limit+offset; record, err = cur.Next() {
+
+        // marshal record into a buffer
+        data, e := record.MarshalBinary()
+        if e != nil {
+            return e
+        }
+
+        // write data
+        _, e = writer.Write(data)
+        if e != nil {
+            return e
+        }
+    }
+
+    return cur.Close()
+}
+
+func (v *versionOneLogFile) State() State {
+    return v.state
+}
+
+func (v *versionOneLogFile) Use(writers ...DecorativeWriteCloser) {
+    if v.state == CLOSED {
+        for _, writer := range writers {
+            v.writeCloser = writer(v.writeCloser)
+        }
+    }
+}
+
+func (v *versionOneLogFile) Append(data []byte) (LogRecord, error) {
+    index, err := v.index.Size()
+    if err != nil {
+        return nil, ErrWriteLogRecord
+    }
+
+    // create log record
+    record, err := v.factory.NewRecord(time.Now().UnixNano(), index, v.flags, data)
+    if err != nil {
+        return nil, ErrWriteLogRecord
+    }
+
+    // binary record
+    buffer, err := record.MarshalBinary()
+    if err != nil {
+        return nil, err
+    }
+
+    // v.fd.Write()
+    _, err = v.fd.Write(buffer)
+    if err != nil {
+        return nil, err
+    }
+
+    v.size += int64(len(buffer))
+    v.index.Append(BasicIndexRecord{record.Time(), record.Index(), int64(v.size)})
+
+    // return newly created record
+    return record, nil
+}
+
+func (v *versionOneLogFile) Cursor() Cursor {
+    return nil
+}
+
+func (v *versionOneLogFile) Snapshot() (Snapshot, error) {
+    return nil, nil
+}
+
+func (v *versionOneLogFile) Metadata() (Metadata, error) {
+    meta := Metadata{}
+    return meta, nil
+}
+
+func (v *versionOneLogFile) Recover() error {
+    return nil
+}
+
+// Close closes the underlying file.
+func (v *versionOneLogFile) Close() error {
+    // Set state as CLOSED
+    v.state = CLOSED
+
+    // sync any last changes to disk
+    err := v.fd.Sync()
+    if err != nil {
+        return err
+    }
+
+    // close file handle
+    return v.fd.Close()
+}
+
+// ## **Utility functions**
+//
+// ### **Creates a version one log file**
 // Creates
 func createVersionOne(file *os.File, filename string, config Config) (WriteAheadLog, error) {
 
@@ -153,205 +334,99 @@ func createVersionOne(file *os.File, filename string, config Config) (WriteAhead
     }
 
     var lock sync.Mutex
-    log := &VersionOneLogFile{lock, file, file, &header, index, record_factory, config.Flags, int64(size), CLOSED}
+    log := &versionOneLogFile{lock, file, file, &header, index, record_factory, config.Flags, int64(size), CLOSED}
 
     return log, nil
 }
 
-func New(filename string, config Config) (WriteAheadLog, error) {
+// ### **Creates a new log file**
+// A new log file is created with a file header consisting of a `LOG` signature
+// followed by an 8-bit version. The file header is then synced to disk and
+// a new log is created.
+// ###### Implentation
+func createNew(file *os.File, filename string, config Config) (WriteAheadLog, error) {
 
-    // return error if config is nil
-    if &config == nil {
-        return nil, ErrConfigRequired
-    }
+    // Create a buffer for the 4-byte file header.
+    // The first 3 bytes are the signature `LOG` followed by an 8-bit version.
+    buf := make([]byte, 4)
 
-    // try to open log file, return error on fail
-    file, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_RDWR, config.FileMode)
+    // Write the `LOG` file signature to the first 3 bytes of the file.
+    xbinary.LittleEndian.PutString(buf, 0, "LOG")
+
+    // Set file version to the given `config.Version`.
+    buf[3] = byte(config.Version)
+
+    // Write the file header buffer to the file.
+    _, err := file.Write(buf)
+
+    // If the header could not be written, close the file and return a
+    // `ErrWriteLogHeader` error along with a `nil` log.
     if err != nil {
-        return nil, err
+        file.Close()
+        return nil, ErrWriteLogHeader
     }
 
-    // get file stat, close file and return on error
-    stat, err := file.Stat()
+    // If writing the file header succeeded, sync the file header to disk.
+    err = file.Sync()
+
+    // If the sync command failed, return a `ErrWriteLogHeader` error and a
+    // `nil` log.
+    if err != nil {
+        return nil, ErrWriteLogHeader
+    }
+
+    // Returns the proper log parser based on the given `config.Version`.
+    return selectVersion(file, filename, config)
+}
+
+// ### **Opens an existing log file**
+// Opens an existing file and returns a log based on the file header. If the
+// file contains a version which is not understood, the error
+// `ErrInvalidFileVersion` is returned along with a `nil` log.
+//
+// If the file header cannot be read, an error is also returned.
+// ###### Implementation
+func openExisting(file *os.File, filename string, config Config) (WriteAheadLog, error) {
+
+    // Create a buffer for the 4-byte file header.
+    // The first 3 bytes are the signature `LOG` followed by an 8-bit version.
+    buf := make([]byte, 4)
+
+    // Read the file header into the buffer
+    _, err := file.ReadAt(buf, 0)
+
+    // If there was an error reading the file header, close the file and return
+    // a nil log and the read error.
     if err != nil {
         file.Close()
         return nil, err
     }
 
-    // get header, close file and return on error
-    buf := make([]byte, 4)
-
-    // create header and log
-    // var header BasicFileHeader
-    var log WriteAheadLog
-
-    // if file already has header
-    if stat.Size() >= HeaderOffset {
-
-        // read file header, close and return upon error
-        _, err := file.ReadAt(buf, 0)
-        if err != nil {
-            file.Close()
-            return nil, err
-        }
-
-        // read version
-        switch buf[3] {
-        case VersionOne:
-
-            return createVersionOne(file, filename, config)
-
-        default:
-            return nil, ErrInvalidFileVersion
-        }
-    } else {
-
-        // write magic string
-        xbinary.LittleEndian.PutString(buf, 0, "LOG")
-
-        // write version
-        buf[3] = byte(VersionOne)
-
-        // write index header to file
-        _, err := file.Write(buf)
-        if err != nil {
-            file.Close()
-            return nil, err
-        }
-
-        // flush data to disk
-        err = file.Sync()
-        if err != nil {
-            return nil, ErrWriteLogHeader
-        }
-
-        // create new log file
-        switch config.Version {
-        case VersionOne:
-
-            return createVersionOne(file, filename, config)
-
-        default:
-            return nil, ErrInvalidFileVersion
-        }
+    // If the header was read sucessfully, verify the file signature matches
+    // the expected signature. If the first 3 bytes do not match `LOG`, return
+    // a `nil` log and a `ErrInvalidFileSignature`.
+    if !bytes.Equal(buf[0:3], LogFileSignature) {
+        return nil, ErrInvalidFileSignature
     }
 
-    // return log file
-    return log, nil
+    // Returns the proper log parser based on the given version
+    config.Version = uint8(buf[3])
+    return selectVersion(file, filename, config)
 }
 
-// VersionOneLogFile
-type VersionOneLogFile struct {
-    lock        sync.Mutex
-    fd          *os.File
-    writeCloser io.WriteCloser
-    header      FileHeader
-    index       LogIndex
-    factory     VersionOneLogRecordFactory
-    flags       uint32
-    size        int64
-    state       State
-}
+// ### **Select log version**
+// `selectVersion` is only here to make the code a bit `DRY`er. It simple
+// returns the proper log file based on the given version.
+// ###### Implementation
+func selectVersion(file *os.File, filename string, config Config) (WriteAheadLog, error) {
 
-func (v *VersionOneLogFile) Open() error {
-    v.state = OPEN
-    return nil
-}
-
-func (v *VersionOneLogFile) Pipe(offset, limit uint64, writer io.Writer) error {
-    // create cursor
-    cur := v.Cursor()
-
-    // iterate over requested record range
-    for record, err := cur.Seek(offset); err != nil && record.Index() < limit+offset; record, err = cur.Next() {
-
-        // marshal record into a buffer
-        data, e := record.MarshalBinary()
-        if e != nil {
-            return e
-        }
-
-        // write data
-        _, e = writer.Write(data)
-        if e != nil {
-            return e
-        }
+    // Open the log file based on the current version of the file.
+    // If the version is unrecognized, a `nil` log is returned as well as an
+    // `ErrInvalidFileVersion` error.
+    switch config.Version {
+    case VersionOne:
+        return createVersionOne(file, filename, config)
+    default:
+        return nil, ErrInvalidFileVersion
     }
-
-    return cur.Close()
-}
-
-func (v *VersionOneLogFile) State() State {
-    return v.state
-}
-
-func (v *VersionOneLogFile) Use(writers ...DecorativeWriteCloser) {
-    if v.state == CLOSED {
-        for _, writer := range writers {
-            v.writeCloser = writer(v.writeCloser)
-        }
-    }
-}
-
-func (v *VersionOneLogFile) Append(data []byte) (LogRecord, error) {
-    index, err := v.index.Size()
-    if err != nil {
-        return nil, ErrWriteLogRecord
-    }
-
-    // create log record
-    record, err := v.factory.NewRecord(time.Now().UnixNano(), index, v.flags, data)
-    if err != nil {
-        return nil, ErrWriteLogRecord
-    }
-
-    // binary record
-    buffer, err := record.MarshalBinary()
-    if err != nil {
-        return nil, err
-    }
-
-    // v.fd.Write()
-    _, err = v.fd.Write(buffer)
-    if err != nil {
-        return nil, err
-    }
-
-    v.size += int64(len(buffer))
-    v.index.Append(BasicIndexRecord{record.Time(), record.Index(), int64(v.size)})
-
-    // return newly created record
-    return record, nil
-}
-
-func (v *VersionOneLogFile) Cursor() Cursor {
-    return nil
-}
-
-func (v *VersionOneLogFile) Snapshot() (Snapshot, error) {
-    return nil, nil
-}
-
-func (v *VersionOneLogFile) Metadata() (Metadata, error) {
-    meta := Metadata{}
-    return meta, nil
-}
-
-func (v *VersionOneLogFile) Recover() error {
-    return nil
-}
-
-// Close closes the underlying file.
-func (v *VersionOneLogFile) Close() error {
-    // Set state as CLOSED
-    v.state = CLOSED
-
-    // sync any last changes to disk
-    err := v.fd.Sync()
-    if err != nil {
-        return err
-    }
-
-    // close file handle
-    return v.fd.Close()
 }
