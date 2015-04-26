eddc54a7574a5bf13411d16319e21a02d484d0d1
eliquious <max.franks@synapse-wireless.com>
2015-04-25 19:12:16 -0500

    Refactoring log interfaces and structs
    - VersionOneLog was moved into `log.go` from `wal.go`. `log.go` seems
    like a better place for log implementations.
    - Moved errors in `log.go` into `wal.go` with the other error types.
    - Added lots of documentation
    - Consolidated WriteAheadLog interface. Moved several one-method interfaces into `WriteAheadLog`
    - Renamed `Cursor` to `LogCursor` becuase it is specific to the log file.
    - Renamed `VersionOneLogRecord` to `BasicLogRecord` as it contains all the inforamtion needed for a `LogRecord`.
      - Future implementations can build on top of it instead of having to write an entirely new `LogRecord` implementation.

--- log.go
+++ log.go
@@ -1,126 +1,115 @@
+// # Log Implementations
 package wallaby
 
 import (
 	"encoding"
-	"errors"
 	"io"
+	"os"
+	"sync"
+	"time"
 
 	"github.com/eliquious/xbinary"
 )
 
-var (
-	ErrRecordTooLarge = errors.New("record is too large")
-
-	ErrInvalidRecordSize = errors.New("invalid record size")
-)
-
-const (
-	// DefaultRecordFlags represents the default boolean flags for each log record.
-	DefaultRecordFlags uint32 = 0
-
-	// DefaultMaxRecordSize is the default maximum size of a log record.
-	DefaultMaxRecordSize = 0xffff
-)
+// ## **Log State**
 
 // State is used to maintain the current status of the log.
 type State uint8
 
-// CLOSED represents a closed log file
+// `CLOSED` represents a closed log file
 const CLOSED State = 0
 
-// OPEN signifies the log is currently open
+// `OPEN` signifies the log is currently open
 const OPEN State = 1
+
+// <br/>
+// ## **Log Interfaces**
+
+// ### **Write Ahead Log**
 
 // WriteAheadLog implements an immutable write-ahead log file with indexes and
 // snapshots.
 type WriteAheadLog interface {
 
-	// Closer allows the log to be closed
-	io.Closer
+	// ###### *State*
 
-	// Creates a new record and appends it to the log.
-	Appender
+	// `State` returns the current state of the log file.
+	State() State
 
-	// Recoverable allows the log to be recovered upon crash
-	Recoverable
+	// ###### *Open*
 
-	// Cursable allows cursors to be created.
-	Cursable
-
-	// Stateful allows the state of the log to be retrieved.
-	Stateful
-
-	// Opener opens the log for appending. Prior to this call the log state
+	// `Open` opens the log for appending. Prior to this call the log state
 	// should be CLOSED. Once this is called State() should return OPEN.
-	Opener
+	Open() error
 
-	// Use provides middleware for the internal io.Writer
+	// ###### *Close*
+
+	// `Close` allows the log to be closed. After this is called, `State`
+	// should return `CLOSED` and future appends should fail.
+	Close() error
+
+	// ###### *Append*
+
+	// `Append` wraps the given bytes array in a Record and returns the record
+	// index or an error
+	Append(data []byte) (LogRecord, error)
+
+	// ###### *Recover*
+
+	// Recover should be called when the log is opened to verify consistency
+	// of the log.
+	Recover() error
+
+	// ###### *Cursor*
+
+	// Creates a new Cursor initialized at index 0
+	Cursor() LogCursor
+
+	// ###### *Use*
+
+	// Use provides middleware for the internal `io.WriteCloser`
 	Use(...DecorativeWriteCloser)
 
-	// Piper allows log records to be transferred to another writer.
-	Piper
+	// ###### *Pipe*
+
+	// Pipe copies the raw byte stream into the given `io.Writer` starting at a
+	// record offset and reading up until the given limit.
+	Pipe(offset, limit uint64, writer io.Writer) error
+
+	// ###### *Snapshot*
 
 	// Snapshot records the current position of the log file.
 	Snapshot() (Snapshot, error)
 
-	// Metadata returns meta data of the log file.
+	// ###### *Metadata*
+
+	// Metadata returns metadata of the log file.
 	Metadata() (Metadata, error)
 }
 
-// DecorativeWriteCloser allows for middleware around the internal file writer.
-type DecorativeWriteCloser func(io.WriteCloser) io.WriteCloser
+// ### **Log Cursor**
 
-// Opener is the interface for Open. Open makes the log available for writing.
-type Opener interface {
-	Open() error
-}
-
-// Stateful is the interface for the State method. State returns the current
-// state of the log file.
-type Stateful interface {
-	State() State
-}
-
-// Recoverable is the interface which wraps the Recover method. Recover should
-// return the log to a stable state.
-type Recoverable interface {
-	// Recover should be called when the log is opened to verify consistency
-	// of the log.
-	Recover() error
-}
-
-type Cursable interface {
-	// Creates a new Cursor initialized at index 0
-	Cursor() Cursor
-}
-
-// Appender appends a new record to the end of the log.
-type Appender interface {
-
-	// Append wraps the given bytes array in a Record and returns the record
-	// index or an error
-	Append(data []byte) (LogRecord, error)
-}
-
-// Cursor allows for quite navigation through the log. All Cursor start at zero
+// LogCursor allows for quite navigation through the log. All Cursor start at zero
 //  and moves forward until EOF.
-type Cursor interface {
+type LogCursor interface {
+
+	// ###### *Seek*
 
 	// Seek moves the Cursor to the given record index.
 	Seek(offset uint64) (LogRecord, error)
 
+	// ###### *Next*
+
 	// Next moves the Cursor forward one record.
 	Next() (LogRecord, error)
+
+	// ###### *Close*
 
 	// Close cursor and any associates file handles
 	Close() error
 }
 
-// Piper copies the raw byte stream into the given io.Writer starting at a
-// record offset and reading up until the given limit.
-type Piper interface {
-	Pipe(offset, limit uint64, writer io.Writer) error
-}
+// ### **Log Header**
 
 // LogHeader is at the front of the log file and describes which version the
 // file was written with as well as any boolean flags associated with the file
@@ -129,54 +118,70 @@ type LogHeader interface {
 	Flags() uint32
 }
 
+// ### **Log Record**
+
 // LogRecord describes a single item in the log file. It consists of a time, an
 // index id, a length and the data.
 type LogRecord interface {
+	// ###### *BinaryMarshaler*
 
-	// Returns LogRecord as a byte array
+	// Converts a LogRecord as a byte array
 	encoding.BinaryMarshaler
+
+	// ###### *BinaryUnmarshaler*
 
 	// Converts a byte array into a LogRecord
 	encoding.BinaryUnmarshaler
 
+	// ###### *Size*
+
 	// Size returns the size of the record data
 	Size() uint32
+
+	// ###### *Flags*
 
 	// Flags returns any boolean flags associated
 	Flags() uint32
 
+	// ###### *Time*
+
 	// Time returns the record timestamp
 	Time() int64
 
+	// ###### *Index*
+
 	// Index returns the record offset in the log
 	Index() uint64
+
+	// ###### *Data*
 
 	// Data returns record data
 	Data() []byte
 }
 
-// VersionOneLogRecordFactory generates log records.
-type VersionOneLogRecordFactory struct {
-	MaxRecordSize int
-}
+// ###### **LogRecordFactory**
+// `LogRecordFactory` generates new `LogRecords` from the arguments.
+type LogRecordFactory func(nanos int64, index uint64, flags uint32, data []byte) (LogRecord, error)
 
-// NewRecord creates a v1 log record. Each record is prefixed with a header of
-// 20 bytes. It consists of an int64 timestamp (nanoseconds unixtime), the
-// record index (uint64), boolean flags, a length and the data.
-func (f *VersionOneLogRecordFactory) NewRecord(nanos int64, index uint64, flags uint32, data []byte) (LogRecord, error) {
+// ###### **BasicLogRecordFactory**
 
-	// checks if record is too large
-	if len(data) > f.MaxRecordSize {
-		return nil, ErrRecordTooLarge
+// `BasicLogRecordFactory` creates a v1 log record. Each record is prefixed
+// with a header. If the record data exceeds the maximum record size, an
+// `ErrRecordTooLarge` error is returned.
+func BasicLogRecordFactory(max_size int) LogRecordFactory {
+	return func(nanos int64, index uint64, flags uint32, data []byte) (LogRecord, error) {
+		if len(data) > max_size {
+			return nil, ErrRecordTooLarge
+		}
+		return BasicLogRecord{uint32(len(data)), nanos, index, flags, data}, nil
 	}
-
-	// return new log record
-	return VersionOneLogRecord{uint32(len(data)), nanos, index, flags, data}, nil
 }
 
-// VersionOneLogRecord represents an element in the log. Each record has a
+// ### **BasicLogRecord**
+
+// `BasicLogRecord` represents an element in the log. Each record has a
 // timestamp, an index, boolean flags, a length and the data.
-type VersionOneLogRecord struct {
+type BasicLogRecord struct {
 	size  uint32
 	nanos int64
 	index uint64
@@ -184,33 +189,43 @@ type VersionOneLogRecord struct {
 	data  []byte
 }
 
+// ###### *Time*
+
 // Time represents when the record was created.
-func (r VersionOneLogRecord) Time() int64 {
+func (r BasicLogRecord) Time() int64 {
 	return r.nanos
 }
 
+// ###### *Index*
+
 // Index is where the record lives in the log.
-func (r VersionOneLogRecord) Index() uint64 {
+func (r BasicLogRecord) Index() uint64 {
 	return r.index
 }
 
+// ###### *Flags*
+
 // Flags returns boolean fields associated with the record.
-func (r VersionOneLogRecord) Flags() uint32 {
+func (r BasicLogRecord) Flags() uint32 {
 	return r.flags
 }
 
+// ###### *Size*
+
 // Size returns the length of the record's data.
-func (r VersionOneLogRecord) Size() uint32 {
+func (r BasicLogRecord) Size() uint32 {
 	return r.size
 }
 
+// ###### *Data*
+
 // Data returns the associated byte buffer.
-func (r VersionOneLogRecord) Data() []byte {
+func (r BasicLogRecord) Data() []byte {
 	return r.data
 }
 
 // MarshalBinary encodes the entire record in a byte array.
-func (r VersionOneLogRecord) MarshalBinary() ([]byte, error) {
+func (r BasicLogRecord) MarshalBinary() ([]byte, error) {
 
 	// create header buffer
 	buffer := make([]byte, 24+r.size)
@@ -233,8 +248,8 @@ func (r VersionOneLogRecord) MarshalBinary() ([]byte, error) {
 	return buffer, nil
 }
 
-func (r VersionOneLogRecord) UnmarshalBinary(buffer []byte) error {
-	if len(buffer) < 24 {
+func (r BasicLogRecord) UnmarshalBinary(buffer []byte) error {
+	if len(buffer) < VersionOneLogRecordHeaderSize {
 		return ErrInvalidRecordSize
 	}
 
@@ -261,4 +276,187 @@ func (r VersionOneLogRecord) UnmarshalBinary(buffer []byte) error {
 	r.data = buffer[24:]
 
 	return nil
+}
+
+// ## **Version One Log File**
+//
+// `versionOneLogFile` is obviously v1 of the WAL. It is the first class which
+// implements the `WriteAheadLog` interface. However, it is not a public
+// type. Wallaby strives to maintain a clean API and hence uses interfaces to
+// abstract specific implementations.
+type versionOneLogFile struct {
+	lock        sync.Mutex
+	fd          *os.File
+	writeCloser io.WriteCloser
+	header      FileHeader
+	index       LogIndex
+	factory     LogRecordFactory
+	flags       uint32
+	size        int64
+	state       State
+}
+
+// ###### *Open*
+
+// `Open` simply switches the state to `OPEN`. If the log is already open, the
+// error `ErrLogAlreadyOpen` is returned.
+func (v *versionOneLogFile) Open() error {
+	if v.state == OPEN {
+		return ErrLogAlreadyOpen
+	}
+
+	v.state = OPEN
+	return nil
+}
+
+// ###### *Pipe*
+
+// `Pipe` will read a `limited` number of the log records beginning at the
+// `offset` given. All the these records will be serialized into a byte array
+// and written to the given `io.Writer`. If there was an error either converting
+// the record or writing to the given `io.Writer`, the encountered error will be
+// returned.
+func (v *versionOneLogFile) Pipe(offset, limit uint64, writer io.Writer) error {
+
+	// Create a new record cursor, closing it when exiting the function.
+	cur := v.Cursor()
+	defer cur.Close()
+
+	// Seek to the given `offset` and read the records until the number of
+	// requested records are read or an error occurs.
+	for record, err := cur.Seek(offset); err != nil && record.Index() < limit+offset; record, err = cur.Next() {
+
+		// Marshal the record into a byte array returning the error if there
+		// is one.
+		data, e := record.MarshalBinary()
+		if e != nil {
+			return e
+		}
+
+		// Write the byte array to the given `writer` returning the error if
+		// there is one.
+		_, e = writer.Write(data)
+		if e != nil {
+			return e
+		}
+	}
+
+	// Success. Return a `nil` error.
+	return nil
+}
+
+// ###### *State*
+
+// `State` allows the log state to be queried by returning the current state of
+// the log.
+func (v *versionOneLogFile) State() State {
+	v.lock.Lock()
+	defer v.lock.Unlock()
+	return v.state
+}
+
+// ###### *Use*
+
+// `Use` allows middleware to modify the log's behavior. `DecorativeWriteClosers`
+// are the vehicles which make this possible. They wrap the internal writer
+// with additional capabilities such as using different buffering strategies.
+func (v *versionOneLogFile) Use(writers ...DecorativeWriteCloser) {
+	v.lock.Lock()
+	defer v.lock.Unlock()
+
+	// Only apply the middleware is the log is `CLOSED`. The log remains
+	// in this state until `Open` is called.
+	if v.state == CLOSED {
+
+		// Iterate over all the `DecorativeWriteClosers` replacing the internal
+		// writer with the new one.
+		for _, writer := range writers {
+			v.writeCloser = writer(v.writeCloser)
+		}
+	}
+}
+
+// ###### *Append*
+
+func (v *versionOneLogFile) Append(data []byte) (LogRecord, error) {
+
+	v.lock.Lock()
+	defer v.lock.Unlock()
+
+	if v.state != OPEN {
+		return nil, ErrLogClosed
+	}
+
+	index := v.index.Size()
+
+	// create log record
+	record, err := v.factory(time.Now().UnixNano(), index, v.flags, data)
+	if err != nil {
+		return nil, ErrWriteLogRecord
+	}
+
+	// binary record
+	buffer, err := record.MarshalBinary()
+	if err != nil {
+		return nil, err
+	}
+
+	// _, err = v.fd.Write(buffer)
+	v.writeCloser.Write(buffer)
+	if err != nil {
+		return nil, err
+	}
+
+	v.size += int64(len(buffer))
+	v.index.Append(BasicIndexRecord{record.Time(), record.Index(), int64(v.size), 0})
+
+	// return newly created record
+	return record, nil
+}
+
+// ###### *Cursor*
+
+func (v *versionOneLogFile) Cursor() LogCursor {
+	return nil
+}
+
+// ###### *Snapshot*
+
+func (v *versionOneLogFile) Snapshot() (Snapshot, error) {
+	return nil, nil
+}
+
+// ###### *Metadata*
+
+func (v *versionOneLogFile) Metadata() (Metadata, error) {
+	meta := Metadata{}
+	return meta, nil
+}
+
+// ###### *Recover*
+func (v *versionOneLogFile) Recover() error {
+	return nil
+}
+
+// ### Close
+
+// `Close` changes the log state to `CLOSED`. The `io.WriterCloser` as well as
+// the underlying file should not recieve any writes after this call has
+// completed. `Append` will return an `ErrLogClosed` after this method is
+// called.
+// ##### Implementation
+func (v *versionOneLogFile) Close() error {
+
+	// The log is locked before closing the file. The log is then
+	// unlocked on return. After the log has been locked the state is set to
+	// `CLOSED`.
+	v.lock.Lock()
+	defer v.lock.Unlock()
+
+	v.state = CLOSED
+
+	// Finally, the record `io.WriteCloser` is closed. The `DecorativeWriteCloser`
+	// is expected to flush all the remaining data. If there is an error during
+	// close, the error bubbles up.
+	return v.writeCloser.Close()
 }
